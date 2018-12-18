f3555b2be39874db55aefc39dd27dec775fe5769
lswhcmcc <411863122@qq.com>
2018-12-18 19:23:55 +0800

    Update JCAppDelegate.h

--- Example/JCMediator/JCAppDelegate.h
+++ Example/JCMediator/JCAppDelegate.h
@@ -2,8 +2,8 @@
 //  JCAppDelegate.h
 //  JCMediator
 //
-//  Created by lswhcmcc on 05/22/2018.
-//  Copyright (c) 2018 lswhcmcc. All rights reserved.
+//  Created by JC on 05/22/2018.
+//  Copyright (c) 2018 lswh. All rights reserved.
 //
 
 @import UIKit;
